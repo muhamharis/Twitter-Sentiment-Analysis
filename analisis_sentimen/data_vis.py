bb420fa1858605308e9f4bb958e42eea732075ea
Muhammad Haris <muhamharis@gmail.com>
2019-11-03 06:44:39 +0700

    Update data_vis.py

--- analisis_sentimen/data_vis.py
+++ analisis_sentimen/data_vis.py
@@ -1,33 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
-
-
-# In[ ]:
-
-
 style.use("ggplot")
 
 fig = plt.figure()
 ax1 = fig.add_subplot(1, 1, 1)
 fig.canvas.set_window_title('Live Graph')
 
-
-
-
-# In[ ]:
-
-
 def animate(i):
-
-    
-    
     pull_data = open("twitter-out.txt", "r").read()
     lines = pull_data.split('\n')
 
@@ -52,29 +32,6 @@ def animate(i):
     plt.xlabel('Number of tweets')
     plt.ylabel('Sentiment score')
     ax1.plot(xar, yar)
-
-
-# In[ ]:
-
-
+    
 ani = animation.FuncAnimation(fig, animate, interval=1000)
 plt.show()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
